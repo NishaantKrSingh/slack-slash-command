91ee96eea4655fcbd655439a657d882bfb0ff890
Nishant Singh <nishaantkrsingh@gmail.com>
2025-09-13 11:45:46 +0530

    minor changes

--- api/test.js
+++ api/test.js
@@ -1,7 +1,3 @@
 export default function handler(req, res) {
-  if (req.method === 'GET') {
-    res.status(200).json({ message: 'API is working!' });
-  } else {
-    res.status(405).send('Method Not Allowed');
-  }
+  res.status(200).json({ message: 'API is working!' });
 }
